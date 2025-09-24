5494174a00965c28513a257c25ed5873405fa055
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-24 16:05:44 +0200

    feat: initial commit for routes/auth.js

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Import auth controller methods
+// const { loginUser } = require('../controllers/authController');
+
+// Route to authenticate a user
+// router.post('/login', loginUser);
+
+module.exports = router;
