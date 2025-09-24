f3e7369c1c833fa5665cb21f8b201ccc269b5cae
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-24 16:05:46 +0200

    feat: initial commit for routes/posts.js

---
+++ routes/posts.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// Import post controller methods
+// const { getPosts, createPost, updatePost, deletePost } = require('../controllers/postController');
+
+// Routes for post management
+// router.get('/', getPosts);
+// router.post('/', createPost);
+// router.put('/:id', updatePost);
+// router.delete('/:id', deletePost);
+
+module.exports = router;
