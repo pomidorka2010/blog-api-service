53e2d82dc6781e9bd68bd7446c2f51fd2750356e
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-24 16:08:04 +0200

    Updated MongoDB connection options to include error handling.

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected successfully.');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
 });
 
 // API Routes (to be created)
