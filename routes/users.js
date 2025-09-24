ee2dd87d6137fd0984edef1722836fc5fcbcd9e8
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-24 16:05:43 +0200

    feat: initial commit for routes/users.js

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Import user controller methods
+// const { createUser } = require('../controllers/userController');
+
+// Route to create a new user
+// router.post('/', createUser);
+
+module.exports = router;
